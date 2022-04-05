9c25202e0fb24fb08fa1ae8b7def01a0c080a7d1
Aykut Fatin <aykut.fatin@abv.bg>
2022-04-05 17:23:38 +0300

    added logic related to loading static files

--- JS-Back-End/cubicle-course-project/app.js
+++ JS-Back-End/cubicle-course-project/app.js
@@ -7,6 +7,8 @@ const app = express();
 app.engine('hbs', engine());
 app.set('view engine', 'hbs');
 
+app.use('/static', express.static('static'));
+
 app.get('/', (req, res) => {
     res.render('index', { title: 'HBS' });
 });
