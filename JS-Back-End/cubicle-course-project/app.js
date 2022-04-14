7a63272cafe98299f643088de0c9fcc6477b9be1
Aykut Fatin <aykut.fatin@abv.bg>
2022-04-14 22:52:07 +0300

    Added services file into cubicle project

--- JS-Back-End/cubicle-course-project/app.js
+++ JS-Back-End/cubicle-course-project/app.js
@@ -1,9 +1,9 @@
-const { init: api } = require('./models/storage');
+const { init: api } = require('./services/storage');
 const PORT = 3000;
 
 (async () => {
     const app = require('express')(); // initialize app
-    
+
     require('./config/express')(app); // configure express
     app.use(await api());
     require('./config/routes')(app); // configure routes
