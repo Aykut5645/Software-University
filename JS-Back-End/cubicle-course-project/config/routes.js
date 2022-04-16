da6338e31a8de5159afe204ae26a08d3db5a0ad6
Aykut Fatin <aykut.fatin@abv.bg>
2022-04-16 20:18:07 +0300

    Implemented all the pages related to authenticate

--- JS-Back-End/cubicle-course-project/config/routes.js
+++ JS-Back-End/cubicle-course-project/config/routes.js
@@ -1,12 +1,14 @@
 const comments = require('../controllers/comments');
 
 const homeController = require('../controllers/homeController');
+const authController = require('../controllers/authController');
 const productController = require('../controllers/productController');
 const accessoryController = require('../controllers/accessoryController');
 
 module.exports = app => {
     app.use('/products', productController);
     app.use('/accessory', accessoryController);
+    app.use('/auth', authController);
     app.use('/', homeController);
 
     app.post('/comments/:cubeId/create', comments.post);
